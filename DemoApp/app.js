ad496cf75fb0a3cac627a2226d977ec185f86de1
Kyle Billemeyer <billemeyer.k@gmail.com>
2014-02-04 00:33:15 -0500

    Sets up socketio on web app.  Fires/receives player join event

--- DemoApp/app.js
+++ DemoApp/app.js
@@ -8,7 +8,13 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var socketio = require('socket.io');
+var dgram = require('dgram');
 
+
+/**
+ * Set up Express web server.
+ */
 var app = express();
 
 // all environments
@@ -34,6 +40,35 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+
+/**
+ * Set up socket io port
+ */
+var server = http.createServer(app);
+var io = socketio.listen(server);
+io.on('connection', function(socket){
+    console.log('Connected: ' + socket);
+    socket.on('player join', function(data){
+        console.log('User joined game: ' + data.timestamp);
+    });
+});
+io.on('disconnect', function(data){
+    console.log('Disconnected: ' + data);
+});
+
+var socket = dgram.createSocket('udp4');
+
+
+
+/**
+ * Fire it up!
+ */
+socket.bind(2999, function(){
+    console.log('Dgram UDP4 socket on port 2999');
+});
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
